a49afb9fe4fcd1352dd061a4cda4e3ab346d145f
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-05-08 10:17:46 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-vsscanf-Bagian7__C/Source.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <conio.h>
+
+/*
+    Source by Microsoft (https://docs.microsoft.com/en-us/cpp)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int call_vsscanf(char* tokenstring, char* format, ...) {
+    int result;
+    va_list arglist;
+    va_start(arglist, format);
+    result = vsscanf(tokenstring, format, arglist);
+    va_end(arglist);
+    return result;
+}
+
+int main(void) {
+    char  tokenstring[] = "15 12 14...";
+    char  s[81];
+    char  c;
+    int   i;
+    float fp;
+
+    // Input various data from tokenstring:
+    // max 80 character string:
+    call_vsscanf(tokenstring, "%80s", s);
+    call_vsscanf(tokenstring, "%c", &c);
+    call_vsscanf(tokenstring, "%d", &i);
+    call_vsscanf(tokenstring, "%f", &fp);
+
+    // Output the data read
+    printf("String    = %s\n", s);
+    printf("Character = %c\n", c);
+    printf("Integer:  = %d\n", i);
+    printf("Real:     = %f\n", fp);
+
+    _getch();
+    return 0;
+}
